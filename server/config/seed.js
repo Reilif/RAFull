51eb9954240778f510be78e2f0e44c6978eecc44
Robin Rasch <robin.rasch@fh-bielefeld.de>
2015-06-22 16:59:41 +0200

    Initial commit

---
+++ server/config/seed.js
@@ -0,0 +1,40 @@
+/**
+ * Populate DB with sample data on server start
+ * to disable, edit config/environment/index.js, and set `seedDB: false`
+ */
+
+'use strict';
+
+var Thing = require('../api/thing/thing.model');
+var User = require('../api/user/user.model');
+
+Thing.find({}).remove(function() {
+  Thing.create({
+    name : 'Erstelle eigene Zertifikate',
+    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.',
+  }, {
+    name : 'Verwalte deine Zertifikate',
+    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
+  },  {
+    name : 'SICHERHEIT',
+    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+  });
+});
+
+User.find({}).remove(function() {
+  User.create({
+    provider: 'local',
+    name: 'Test User',
+    email: 'test@test.com',
+    password: 'test'
+  }, {
+    provider: 'local',
+    role: 'admin',
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: 'admin'
+  }, function() {
+      console.log('finished populating users');
+    }
+  );
+});
