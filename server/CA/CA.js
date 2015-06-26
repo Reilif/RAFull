c9d7cae246156ea7f94c4c34a84e0ccd569dc556
Robin Rasch <robin.rasch@fh-bielefeld.de>
2015-06-26 10:20:53 +0200

    Für Marcel

---
+++ server/CA/CA.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Robin on 26.06.2015.
+ */
+
+
+//Methoden zur CA-Verbindung
+
+/**
+ * Erzeugt das Client Certificate
+ */
+function createClientCertificate(user) {
+
+}
+
+/**
+ * Erzeugt das Certificate
+ */
+function createCertificate(user) {
+
+}
+/**
+ * Revoced Certificate
+ */
+function revokeCertificate(certificate) {
+
+}
